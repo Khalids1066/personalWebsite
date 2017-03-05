28a6c0e3ed17c2181ae80d3eb610c3084f75f7da
Khalids1066 <khalid.saeed1066@gmail.com>
2017-03-06 02:11:24 +1300

    New design, new color scheme, new background

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,22 @@
-$(docuemnt).ready(function(){
+$(document).ready(function(){
 
+	$(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+
+	$("#homeBtn, #logo").click(function(){
+		$("#portfolioDiv").addClass('hidden');
+		$("#contactDiv").addClass('hidden');
+		$("#homeDiv").removeClass('hidden');
+	});
+	$("#portfolioBtn").click(function(){
+		$("#homeDiv").addClass('hidden');
+		$("#contactDiv").addClass('hidden');
+		$("#portfolioDiv").removeClass('hidden');
+	});
+	$("#contactBtn").click(function(){
+		$("#homeDiv").addClass('hidden');
+		$("#portfolioDiv").addClass('hidden');
+		$("#contactDiv").removeClass('hidden');
+	});
 });
